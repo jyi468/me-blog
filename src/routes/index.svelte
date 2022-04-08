108005fc690e04e263dee6efe8281f2adca2f92e
jyiwm <joshua.yi@warnermedia.com>
2022-04-08 15:09:58 -0400

    Fetch data from posts API endpoint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
+<script context="module">
+    // This module runs before the page loads
+    export const load = async ({fetch}) => {
+        const response = await fetch('/posts.json');
+        if (response.ok) {
+            const posts = await response.json();
+            return {
+                props: {posts}
+            };
+        }
+    };
+</script>
+
+<script>
+    export let posts;
+</script>
+
+<pre>{JSON.stringify(posts, null, 2)}</pre>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
