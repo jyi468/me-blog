6d41c5e72bef680f4b0da7b4a554bea2fa6e8ed6
jyiwm <joshua.yi@warnermedia.com>
2022-04-09 13:19:20 -0400

    Add tags with if condition

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -56,3 +56,19 @@
 <p class="text-secondary text-xs tracking-widest font-semibold">
     {new Date(date).toDateString()}
 </p>
+
+<div class="mb-5 flex justify-between">
+    <div>
+        {#if tags}
+            <div class="mt-5 space-x-2">
+                {#each tags as tag}
+                    <span class="badge badge-primary">{tag}</span>
+                {/each}
+            </div>
+        {/if}
+    </div>
+</div>
+
+<article div class="prose">
+    {@html html}
+</article>
